7be957c2ed88752fe7eead92ffd17120b081d334
Hoon Lee <yl.hoony@gmail.com>
2018-04-13 01:35:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function showCommits(e) {
 }
 
 function renderCommits(data) {
-  console.log("renderCommits:", data)
+  console.log("renderCommits:", data);
 }
 
 function parseJSON(res) {
@@ -47,20 +47,8 @@ $(document).ready(function (){
 
 
 //
-// var renderCommit = (commit) => {
-//   return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
-// }
-//
 // var renderCommits = (data) => {
 //   let result = data.map((commit)=>renderCommit(commit)).join('')
 //   return `<ul>${result}</ul>`
-// }
-//
-// var showCommits = (el) => {
-//   $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
-//     $('#details').html(renderCommits(data))
-//   }).fail(error => {
-//     displayError()
-//   })
 // }
 //
